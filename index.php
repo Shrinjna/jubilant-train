088013b8cef0f8bb431daeb4b9fd400f87b7e04f
Shrinjna <sushmitathakur1993@gmail.com>
2016-08-27 21:06:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sushmita Thakur</title>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<link rel="shortcut icon" type="image/png" href="favicon/ICQ_Flower_Icon_by_fliuwoek.png"/>
+<link rel="shortcut icon" type="image/png" href="http://eg.com/favicon/ICQ_Flower_Icon_by_fliuwoek.png"/>
+</head>
+<body>
+<i>
+
+<!--Home-->
+<div style="font-size:55px; color:#FFFFFF; top:25%; left:33%; position:absolute;" class="title_back">&nbsp;&nbsp; Sushmita Thakur &nbsp;&nbsp;</div>
+ <div style=" top:34%; left:40%; position:absolute; color:#FF6699;">I love
+  <span data-id="1">Code</span>
+  <span data-id="2">Pizza</span>
+  <span data-id="3">Dogs</span>
+  <span data-id="4">Science</span>
+  <span data-id="5">People</span>
+  <span data-id="6">Me</span>
+</div>
+
+
+<!--Academics-->
+<div class="parallax"></div>
+<div id="academics" class="wrapper">
+     
+     <div class="heading1" style="top:2%;">Academics</div><br>
+     <div id="chart1" class="chart1"></div>
+     <div id="chart2" class="chart2"></div>
+     <div id="chart3" class="chart3"></div>
+      
+     <div class="chart-text1">
+        <div id="per-text">81.98%</div><br>
+        <div id="sub-text" >2015</div>
+        <div id="text">B.E. (Computer Science Engineering)</div>
+        <div id="sub-text" >Shri Shankracharya Technical Campus</div>
+        <div id="sub-text">Bhilai,(C.G.)</div>
+     </div>
+     
+      <div class="chart-text2">
+         <div id="per-text">83.8%</div><br>
+         <div id="sub-text">2011</div>
+         <div id="text">12 <sup>th</sup></div>
+         <div id="sub-text">Shri Mahaveer Jain Vidyalaya</div>
+         <div id="sub-text">Durg,(C.G.)</div>
+      </div>
+     
+      <div class="chart-text3">
+         <div id="per-text">90%</div><br>
+         <div id="sub-text"  >2009</div>
+         <div id="text"> 10 <sup>th</sup></div>
+         <div id="sub-text">Shri Mahaveer Jain Vidyalaya</div>  
+         <div id="sub-text">Durg,(C.G.)</div>
+       </div>
+     
+    <div style="text-align:center;color:#FF6699;"><br><br><br><br><br><br><br><br><br><br>
+       Received PRIME MINISTER TROPHY SCHOLARSHIP from BHILAI STEEL PLANT (C.G.).
+      <br><br>Got honored by STATE BANK OF INDIA for scoring 90% marks on 10th board 2009.
+     </div>
+      
+</div>
+
+<!--Projects-->
+<div class="parallax"></div>
+<div id="projects" class="wrapper">
+<br><br>
+<!--  <div class="heading1" style="top:230%">Projects</div>-->
+  <div class="heading1">Projects</div>
+ <!--<div class="project-head" style="top:240%;">-->
+ <div class="para-text">
+    
+      <div style="color:#FF6699; ">Blood-group Store & Consultancy System</div>
+       <br><br>A blood group availability search System with few extra functionalities other than the main consultancy module.
+         <br>
+         Technology Used: HTML, CSS, Javascript, PHP
+     <br><br> 
+     
+     <div style="color:#FF6699; ">Funmail</div>
+     <br>A website for sending E-mails with the focus on the front end at how website communicates its purpose to the user.The user could register into the website and send emails with one attachment (.doc, .pdf , .jpg etc.) at a time.
+      <br>
+      Technology Used: HTML, CSS, Javascript, PHP
+     <br><br>
+     
+     <div style="color:#FF6699; ">Slam Book</div>
+     <br><br>Something I had just made for fun.
+     <br>
+     Technology Used: HTML, CSS, Javascript, PHP
+     
+
+  </div>
+
+</div>
+
+<!--Experience-->
+<div class="parallax"></div>
+<div id="experience" class="wrapper">
+   <br><br>
+  <div class="heading1">Work</div>
+ <br><br>
+   <div class="para-text"  style="color:#FF6699;">
+              Company name:  Aspricot Technologies<br>
+              Job title: PHP Developer (Wordpress)  <br>
+              Duration: 3 Months ( January 2016 – April 2016)<br>
+              Location: Indore, MP (India)
+       <br><br><br>
+        <div style="color:#FFFFFF;">My responsibilities as a Php developer in this Web development company was to transform static websites into dynamic Wordpress site with the objective to write sophisticated Wordpress format PHP codes and have an excellent product ready for the client before the deadline.</div>
+ </div>
+
+</div>
+
+<!--Extra-Curricular-->
+<div class="parallax"></div>
+<div id="co-curr" class="wrapper">
+   <br><br>
+  <!--<div class="heading1" style="top:550%">Certifications</div>-->
+   <div class="heading1" style="color:#FF6699;">Certifications</div>
+  <div class="kathak-slide" >
+   Successful completion of 2 years KATHAK course from Indira Kla Sangeet Vishwavidyalaya, Khairagarh (C.G.) (2011-2013).
+   <br><br>
+   Got CERTIFICATE OF EXECELLENCE from Krishna Priya Kathak Kendra in association with Delhi Kathak Kendra (Delhi) in 5 days workshop by Pt. Krishna Mohan Maharaj ji (2013)
+  </div>
+
+</div>
+<!--Volunteering-->
+<div class="parallax"></div>
+<div id="volunteering" class="wrapper">
+
+<!--  <div class="heading1" style="top:705%; color:#FFFFFF">Volunteering Work</div>-->
+   <br><br>
+   <div class="heading1" style="color:#FFFFFF">Volunteering Work</div><br><br>
+   <br><br><br><br><br><br>
+   <div class="para-text"  style="color:#FF6699;">
+    <!--  <div class="project-head"  style="top:720%; color:#FFFFFF;">-->
+   Name of the group – Yuva Ekta Sangthan (Y.E.S.) , Durg (C.G.)<br><br><br><br>
+    <div style="color:#FFFFFF;"> A non-profitable group of young people who are ambitious to make the a world better place, starting from their home town.<br>
+     We started with cleaning our town several times to set an example for the people, started “Swachchhta Abhiyaan” in local area and currently we provide free tuitions to children who can’t afford to pay private tuition fees.
+     </div>
+
+   </div>
+
+</div>
+
+<!--About-->
+<div class="parallax"></div>
+<div id="about" class="wrapper">
+<!-- <div class="heading1" style="top:860%;color:#FFFFFF;">About</div>-->
+ <br><br>
+ <div class="heading1" style="color:#FFFFFF;">About</div>
+ <br>
+ <img src="images/Sushmita.jpg" class="my-picture"><br>
+ <div style="color:#FF6699;text-align:center;width:60%;margin-left:25%;margin-right:25%;">
+ I am a self-motivated individual, who is looking forward to working on her foundations of leadership and programming skills. I have experience in managing loads of work from the events I organised in the past.<br><br> I am diligent, have a winning attitude with people and am a person capable of thinking on her feet. I love working in an environment where no two days are the same. 
+</div> <br><br>
+   <div class="heading1" style=" color:#FFFFFF;">Contact</div>
+   <div class="para-text" style=" text-align:center;">
+     <br>Drop me a mail on <div style="color:#FF6699;">sushmitathakur1993@gmail.com</div>
+    <br>Check out my Linkedin profile - <div><a style="color:#FF6699;" href="https://in.linkedin.com/pub/sushmita-thakur/bb/429/a91">https://in.linkedin.com/pub/sushmita-thakur/bb/429/a91</a></div> 
+
+   </div>
+</div>
+
+</i></body>
+ 
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script src="js/radial-progress-bar.js"></script>
+<script src="js/percentage.js"></script>
+ 
+</html>
